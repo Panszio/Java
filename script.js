20f6dff5b9e82079ebd35030651205ef296a70a4
Panszio <118809779+Panszio@users.noreply.github.com>
2024-10-13 20:08:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let x = 5;
+
+ console.log(5 == 5);   //porównanie
+console.log(10 === 10);// sprawdza też typ zmienej
+
+console.log(5 != 10); // sprawdza wartość liczby czy są rórzne i typy
+console.log(5 !== "5");
+
+console.log(5 < 5);
+console.log(5 >= 5);
+console.log(5 < 5);
+
+// OPERATORY LOGICZNE
+
+console.log(5 >= 5 || 5 == 1); // logiczny LUB 1 true = true
+console.log(true && false); //  logiczny I potrzebuje 2 true = true
+console.log(!5> 0);
+
+console.log(true || true && false);
